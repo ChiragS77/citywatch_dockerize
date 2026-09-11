ece010c8f98bfb8cd83df619ac17369b102e6a5d
Chirag Deshmukh <deshmukhchirag178@gmail.com>
2026-09-11 22:28:42 +0530

    register_commit

--- citywatch_app/src/app/data/location-data.ts
+++ citywatch_app/src/app/data/location-data.ts
@@ -1,39 +1,55 @@
-export const LOCATION_DATA: any = {
+// export const LOCATION_DATA: any = {
 
-  Maharashtra: {
+//   Maharashtra: {
 
-    Pune: {
+//     Pune: {
 
-      Haveli: ["Ward 1","Ward 2","Ward 3","Ward 4"],
+//       Haveli: ["Ward 1","Ward 2","Ward 3","Ward 4"],
 
-      Mulshi: ["Ward 1","Ward 2"],
+//       Mulshi: ["Ward 1","Ward 2"],
 
-      Baramati: ["Ward 1","Ward 2"]
+//       Baramati: ["Ward 1","Ward 2"]
 
-    },
+//     },
 
-    Nagpur: {
+//     Nagpur: {
 
-      "Nagpur Urban": ["Ward 1","Ward 2","Ward 3"],
+//       "Nagpur Urban": ["Ward 1","Ward 2","Ward 3"],
 
-      Hingna: ["Ward 1","Ward 2"]
+//       Hingna: ["Ward 1","Ward 2"]
 
-    },
+//     },
 
-    Nashik: {
+//     Nashik: {
 
-      Nashik: ["Ward 1","Ward 2"],
+//       Nashik: ["Ward 1","Ward 2"],
 
-      Sinnar: ["Ward 1","Ward 2"]
+//       Sinnar: ["Ward 1","Ward 2"]
 
-    }
+//     }
 
-  },
+//   },
 
-  Gujarat: {},
+//   Gujarat: {},
 
-  Rajasthan: {},
+//   Rajasthan: {},
 
-  "Madhya Pradesh": {}
+//   "Madhya Pradesh": {}
 
-};
+// };
+
+export const LOCATION_DATA: any = { Maharashtra: 
+  { Pune: 
+    { Haveli: [1, 2, 3, 4],
+      Mulshi: [1, 2],
+       Baramati: [1, 2] },
+        Nagpur: { "Nagpur Urban": [1, 2, 3], 
+          Hingna: [1, 2] }, 
+          Nashik: 
+          { Nashik: [1, 2], 
+            Sinnar: [1, 2] } },
+
+ Gujarat: {}, 
+ Rajasthan: {}, 
+ "Madhya Pradesh": {}
+ };
